e7b91308fa6075b5e3f1effc84438f1d566af377
sadsfd <sdfsdf>
2026-06-17 19:05:25 +0200

    pathes

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
-export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/hei-vibe-mar/" : "/"
-}));
+export default defineConfig({
+  base: "./"
+});
